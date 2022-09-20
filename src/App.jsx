e726e7686872c31dbe25f60e5b7312a5436a788b
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-09-20 21:26:01 +0530

    added prev and next button css

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,50 @@ import "./style/style.css";
 
 function App() {
   const [data, setData] = useState(people);
+  const [index, setIndex] = useState(0);
+
+  const prevButton = () => {
+    setIndex((previousIndex) => {
+      let newIndex = previousIndex + 1;
+
+      if (newIndex < 0) {
+        index = data.length - 1;
+      }
+      return newIndex;
+    });
+  };
+  const nextButton = () => {
+    setIndex((previousIndex) => {
+      let newIndex = previousIndex - 1;
+
+      if (newIndex > data.length - 1) {
+        index = 0;
+      }
+      return newIndex;
+    });
+  };
 
   return (
-    <main>
+    <main className="main">
       <section className="review__title">
         <h1 className="review__heading">Reviews</h1>
-        <section className="review__card-container">
-          {data.map((ele) => {
+        <section className="review__card--container">
+          {data.map((ele, personIndex) => {
             const { id, image, name, title, quote } = ele;
+            let position = "active";
+            if (personIndex === index) {
+              position = "next";
+            }
+
+            if (
+              personIndex == index - 1 ||
+              (index === 0 && personIndex === data.length - 1)
+            ) {
+              position = "last";
+            }
+
             return (
-              <article className="review__card" key={id}>
+              <article className={`review__card   ${position}`} key={id}>
                 <img
                   src={image}
                   alt="review__image"
@@ -25,10 +59,16 @@ function App() {
                 <h3 className="review__title">{title}</h3>
                 <p className="review__quote">{quote}</p>
                 <article className="review__btn--group">
-                  <button className="btn btn__left">
+                  <button
+                    className="btn btn__left"
+                    onClick={() => prevButton()}
+                  >
                     <FaAngleDoubleLeft />
                   </button>
-                  <button className="btn btn__right">
+                  <button
+                    className="btn btn__right"
+                    onClick={() => nextButton()}
+                  >
                     <FaAngleDoubleRight />
                   </button>
                 </article>
